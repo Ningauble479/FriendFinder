67ec7be3b4096ebe362c02b2591b552cbee0ad87
Devon <Dvowen@cox.net>
2019-08-15 10:14:41 -0700

    change

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiroutes")(app);
+require("./app/routing/htmlroutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
